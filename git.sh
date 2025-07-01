5a75d5c194676291890e68f7e33e5166f7b75784
Ayyash <ayasabdullah4@gmail.com>
2025-07-01 17:00:37 +0700

    Initial commit

---
+++ git.sh
@@ -0,0 +1,5 @@
+git remote add origin https://github.com/abdullahayas123/assignment_dibimbing_7.git
+git add .
+git commit -m "Initial commit"
+git branch -M main  # Rename to main branch if needed
+git push -u origin main
